e205e600c719837dbd83983a5f3c2a3813f60ef3
Luigi Amorfini <107876389+luigi-amorfini@users.noreply.github.com>
2022-06-27 17:22:55 +0200

    Update calcola_iva.py

--- calcola_iva.py
+++ calcola_iva.py
@@ -8,7 +8,7 @@ def main():
     iva = InputOutput().input_int('Iva : ', "Inserire l'iva")
     r = Finance().calculate_tax(prezzo, iva)
     prezzo_con_iva = prezzo + round(r, 2)
-    print("Prezzo : {}, Iva : {} , Prezzo con Iva {}".format(prezzo, iva, prezzo_con_iva))
+    print("Prezzo : {}, Iva : {} , Prezzo con Iva : {}".format(prezzo, iva, prezzo_con_iva))
 
 
 if __name__ == '__main__':
